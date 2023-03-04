239fc6e0de2d9568c73bce1bd8695e8de1979645
dankob <dankob@jimp.iem.pw.edu.pl>
2023-03-04 23:44:57 +0100

    latwiejszy-add

--- Makefile
+++
@@ -1,6 +0,0 @@
-test: main.o SzyfrXOR/libxor.a
-	$(CC) -o main main.o -L SzyfrXOR -l xor
-main.o: main.c
-	$(CC) -c main.c
-clean:
-	rm *.o main
--- SzyfrXOR/szyfr.h
+++
@@ -1,7 +0,0 @@
-#ifndef SZYFR_H_INCLUDED
-#define SZYFR_H_INCLUDED
-
-void XOR(FILE *input, FILE *output, int char_number);
-int algorithm (int A, int B);
-void print_files(FILE *input, FILE *output);
-#endif
