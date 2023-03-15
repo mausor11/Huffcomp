126a582cf7a1ce52e7012981e657ec6a2436eac0
crppl <janmachowski69@gmail.com>
2023-03-15 21:56:48 +0100

    basic funkcje

--- smieszne_testy_drzewa.c
+++ smieszne_testy_drzewa.c
@@ -2,27 +2,85 @@
 #include <stdlib.h>
 
 typedef struct e {
-	char a;
+	char znak;
 	struct e *wyz;
 	struct e *niz;
 
 } *d_t;
 
-int main(int argc, char **argv) {
-	FILE *in = fopen(argv[1], "rb");
-	char c;
-	d_t tab = malloc(sizeof *tab);
-	void *const ptr = tab;
-	tab->wyz = NULL;
-	tab->niz = NULL;
-	fread(&(tab->a), sizeof(char), 1, in);
-	if(in == NULL) { fprintf(stderr, "cep\n");return 1; }
-	while((fread(&c, sizeof(c), 1, in)) == sizeof(c) ){
-		tab = ptr;
-		if(c > tab->a) {
-			printf("bigga\n");
-		} else {printf("smolla/saem\n");}
+
+d_t createTree() {
+	d_t neww = malloc(sizeof *neww);
+	neww->wyz = NULL;
+	neww->niz = NULL;
+	return neww;
+}
+
+void freeTree(d_t tree){
+	if(tree->niz != NULL) {
+		freeTree(tree->niz);
 	}
-	fclose(in);
+	if(tree->wyz != NULL) {
+		freeTree(tree->wyz);
+	}
+	free(tree);
+}
+
+
+d_t extend(d_t tree, char what, char where) {
+	d_t bab = createTree();
+	bab->znak = what;
+	switch(where) {
+		case 'r':
+			bab->wyz = tree;
+			break;
+		case 'l':
+			bab->niz = tree;
+			break;
+		case 'p':
+			if(tree->wyz == NULL) {
+				tree->wyz = bab;
+				bab = tree;
+			}
+			break;
+		default:
+			if(tree->niz == NULL) {
+				tree->niz = bab;
+				bab = tree;
+				fprintf(stdout, "%c, %c\n", bab->znak, tree->znak);
+			}
+			break;
+
+	}
+	return bab;
+}
+
+
+
+int main(int argc, char **argv) {
+//	FILE *in = fopen(argv[1], "rb");
+	d_t tab = createTree();
+	void *const ptr = tab;
+	tab->znak = 'a';
+	d_t ntab = extend(tab, 'b', 'r');
+	tab = extend(tab, 'c', 'n');
+	tab = ptr;
+	if(ntab->wyz != NULL)
+		printf("dziaa: %c -> %c\n", ntab->znak, ntab->wyz->znak);
+	if(tab->niz != NULL)
+		printf("dziaa drugie: %c -> %c -> %c\n", ntab->znak, ntab->wyz->znak, ntab->wyz->niz->znak);// tab->niz->wyz->znak);
+
+	/*
+	co tu się wydarzylo:
+	powstalo drzewo, najwyzszy wskaznik to ntab:
+		ntab->znak = b
+		ntab->niz = null
+		ntab->wyz->znak = tab->znak = a
+		ntab->wyz->niz->znak = ntab->niz->znak = c
+		reszta null
+
+	*/
+
+	freeTree(ntab);
 	return 0;
 }
