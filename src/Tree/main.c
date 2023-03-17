5b973a1cb88568ca2eccb3953f6e547c14fa0397
crppl <janmachowski69@gmail.com>
2023-03-17 21:26:21 +0100

    work in progress

--- src/Tree/main.c
+++ src/Tree/main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     char x;
 
     test = charCounter(in, test, true);
-    readTree(test);
+    writeTree(test);
     freeTree(test);
 
 
--- src/Tree/tree.c
+++ src/Tree/tree.c
@@ -3,23 +3,22 @@
 #include <stdbool.h>
 
 #include "tree.h"
+#include "bitbajt.h"
 
 d_t createTree() {
-    d_t neww = malloc(sizeof *neww);
-    neww->right_node = NULL;
-    neww->left_node = NULL;
-    neww->counter = 0;
-    return neww;
+	d_t neww = malloc(sizeof *neww);
+	neww->right_node = NULL;
+	neww->left_node = NULL;
+	neww->counter = 0;
+	return neww;
 }
 
 void freeTree(d_t tree) {
-    if(tree->left_node != NULL) {
-        freeTree(tree->left_node);
-    }
-    if(tree->right_node != NULL) {
-        freeTree(tree->right_node);
-    }
-    free(tree);
+	if(tree->left_node != NULL)
+		freeTree(tree->left_node);
+	if(tree->right_node != NULL)
+		freeTree(tree->right_node);
+	free(tree);
 }
 
 
@@ -53,42 +52,63 @@ d_t extend(d_t tree, char what, char where) {
 
 d_t add(d_t tree, char znak) {
 
-    if(tree == NULL) {
-        d_t n_tree = createTree();
-        n_tree->znak = znak;
-        n_tree->counter = 1;
-        return n_tree;
-    }
+	if(tree == NULL) {
+		d_t n_tree = createTree();
+		n_tree->znak = znak;
+		n_tree->counter = 1;
+		return n_tree;
+	}
 
-    if(znak < tree->znak) {
-        tree->left_node = add(tree->left_node, znak);
-    } else if (znak > tree->znak) {
-        tree->right_node = add(tree->right_node, znak);
-    } else {
-        tree->counter++;
-    }
-
-    return tree;
+	if(znak < tree->znak) {
+		tree->left_node = add(tree->left_node, znak);
+	} else if (znak > tree->znak) {
+		tree->right_node = add(tree->right_node, znak);
+	} else {
+		tree->counter++;
+	}
+	return tree;
 }
 
 d_t charCounter (FILE *input, d_t tree, bool Verbose) {
-    char x;
-    while((x = fgetc(input)) != EOF) {
-        tree = add(tree, x);
-    }
-    return tree;
+	char x;
+	while((x = fgetc(input)) != EOF) {
+		tree = add(tree, x);
+	}
+	return tree;
 }
 
 
-void readTree(d_t tree) {
-
-    if(tree != NULL) {
-
-        readTree( tree->left_node);
-        readTree( tree->right_node);
-        printf("%c ascii:%d -> %d\n", tree->znak, tree->znak, tree->counter);
-
-    }
-
+void writeTree(d_t tree) {
+	if(tree != NULL) {
+		writeTree( tree->left_node);
+		writeTree( tree->right_node);
+		printf("%c ascii:%d -> %d\n", tree->znak, tree->znak, tree->counter);
+	}
 }
 
+
+
+
+int readTree(d_t tree, char *tab, int many) {
+	union eitbit unia;
+	int i = 0;		// całokształtne przesunięcie
+	int twobit = 0;		// temp na dwa bity operacyjne
+	int x = 0;		// cykliczne przesunięcie
+//	char c;			// temp do wyjęcia danych
+	do {
+		//c = *(tab+i);
+		unia.A = *(tab+i);
+		unia.B = *(tab+i+1);
+		unia.shrt << x;
+		twobit = 10*bit(unia.A, 7) + bit(unia.B, 8);
+
+		if(twobit == 11) {		// oba równe 1
+			
+			return i;
+		}
+		x = (x+2)%8;
+		i++;
+	}while( i < many - 1);
+	return -1;
+
+}
